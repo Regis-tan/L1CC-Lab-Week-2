ae4506f2f278102faa03b8092995c30bfef6ed67
Regis-tan <144893732+Regis-tan@users.noreply.github.com>
2023-09-21 20:39:08 +0700

    Update L1CC Lab Assignment Week 2.py
    improved code for acceleration calculator

--- L1CC Lab Assignment Week 2.py
+++ L1CC Lab Assignment Week 2.py
@@ -23,7 +23,7 @@ def number_calculator():
     value_input = input("Input values (x operator y):")
     value_input_split = value_input.split()
     if len(value_input_split) == 3:
-        x, operator, y = value_input_split
+        x,operator,y = value_input_split
         try:
             x = float(x)
             y = float(y)
@@ -56,18 +56,19 @@ def acceleration_calculator():
     print("="*40)
     print("Acceleration calculator has been picked.")
     value_input = input("Input values (Kg Newtons):")
-    if " " in value_input:
-        x,y = value_input.split(" ")
+    value_input_split = value_input.split()
+    if len(value_input_split) == 2:
         try:
+            x,y = value_input_split
             x = float(x)
             y = float(y)
-            print(f"Acceleration is {y/x} m/s^2")
+            print(f"Acceleration is {y / x} m/s^2")
         except ValueError:
             print("Syntax error: value must be a number.")
     else:
-        print("Syntax error: expression must be in form of 'x y' (with spacing).")
+        print("Syntax error: expression must be in form of 'x y' (with spacing). ")
     repeat_calculator = input("Calculate again? (y/n):")
-    if  repeat_calculator == "y":
+    if repeat_calculator == "y":
         acceleration_calculator()
     elif repeat_calculator == "n":
         worthless_idiot_stupid_dumb_unessecary_garbage_silly_disgrace_variable = 1
